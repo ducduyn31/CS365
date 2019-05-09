b71496a0349700ee1e464de34fe72956e6fa0158
Daniel Ng <ducduy.ababat@gmail.com>
2019-05-09 15:34:17 +0700

    Palindrome algo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int len = 0;
+    cout << "Length of string: ";
+    cin >> len;
+
+    char mayBePalindrome[len+1];
+    cin.getline(mayBePalindrome, len);
+    mayBePalindrome[len] = 0;
+
+    int mid = len / 2;
+    int i = 0;
+    bool isPalindrome = true;
+    while ( i < mid) {
+        if (mayBePalindrome[i] != mayBePalindrome[len-1-i]){
+            isPalindrome = false;
+            break;
+        }
+        i += 1;
+    }
+
+    if (isPalindrome) cout << "Is palindrome\n";
+    else cout << "Not palindrome\n";
+
     return 0;
 }
