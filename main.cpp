e8dadf839eef44d562ff3437ac571dc4a6d4ae19
Daniel Ng <ducduy.ababat@gmail.com>
2019-05-09 16:05:28 +0700

    binary

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int i;
+    cout << "Enter 1 number: ";
+    cin >> i;
+
+    unsigned int mask = 1 << 31;
+    for (int j = 0; j < 32; ++j) {
+        if ((i & mask) == mask) cout << '1';
+        else cout << '0';
+
+        mask = mask >> 1;
+    }
     return 0;
 }
