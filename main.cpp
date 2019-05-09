39dee4cde2f0ed237a525c23b2791b3bfc117f24
Daniel Ng <ducduy.ababat@gmail.com>
2019-05-09 16:38:42 +0700

    pyramid5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int rows = 0;
+    cout << "How many rows: ";
+    cin >> rows;
+
+    int cols = rows * 2 -1;
+    int midRow = cols / 2;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols ; ++j) {
+            int distanceToMid = abs(j - midRow);
+            int val = i - distanceToMid;
+            if ( val < 0 ) {
+                cout << ' ';
+                continue;
+            }
+            cout << val + 1;
+        }
+        cout << '\n';
+    }
     return 0;
 }
