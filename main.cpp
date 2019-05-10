4acfa0636fbaccead552efe1c024b8c02ccc756b
Daniel Ng <ducduy.ababat@gmail.com>
2019-05-10 22:40:53 +0700

    init

--- main.cpp
+++ main.cpp
@@ -1,25 +1,10 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
 int main() {
-    const int SIZE = 256;
-    char str[SIZE] = {0};
 
-    for (;;) {
-        cout << "Enter a string: ";
-        cin.getline(str, SIZE);
-        int len = strlen(str);
-        if (len == 0 || len > 20) break;
-
-        char temp[len + 1];
-        for (int i = 0; i < len; ++i) {
-            temp[i] = str[len - i - 1];
-        }
-        temp[len] = 0;
-        cout << temp << endl;
-    }
+    cout << "Hello, World" << endl;
 
     return 0;
 }
