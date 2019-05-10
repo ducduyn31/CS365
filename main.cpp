7aaad4509609833c7b7c4801e036fa0d9bd65670
Daniel Ng <ducduy.ababat@gmail.com>
2019-05-10 22:37:05 +0700

    reverse

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World" << endl;
+    const int SIZE = 256;
+    char str[SIZE] = {0};
+
+    for (;;) {
+        cout << "Enter a string: ";
+        cin.getline(str, SIZE);
+        int len = strlen(str);
+        if (len == 0 || len > 20) break;
+
+        char temp[len + 1];
+        for (int i = 0; i < len; ++i) {
+            temp[i] = str[len - i - 1];
+        }
+        temp[len] = 0;
+        cout << temp << endl;
+    }
+
     return 0;
 }
