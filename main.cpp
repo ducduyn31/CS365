832544415be4edc04f6e457460e4344362d280fa
Daniel Ng <ducduy.ababat@gmail.com>
2019-05-10 20:26:30 +0700

    init

--- main.cpp
+++ main.cpp
@@ -3,24 +3,6 @@
 using namespace std;
 
 int main() {
-    int rows = 0;
-    cout << "How many rows: ";
-    cin >> rows;
-
-    int cols = rows * 2 -1;
-    int midRow = cols / 2;
-
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols ; ++j) {
-            int distanceToMid = abs(j - midRow);
-            int val = i - distanceToMid;
-            if ( val < 0 ) {
-                cout << ' ';
-                continue;
-            }
-            cout << val + 1;
-        }
-        cout << '\n';
-    }
+    cout << "Hello, World" << endl;
     return 0;
 }
